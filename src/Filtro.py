998ee50ed6d1d86f8bf32fa4175b4b8bd45ef432
Gileno cordeiro <gileno.cordeiroc@gmail.com>
2020-12-17 08:26:04 -0300

    Ajustes Responsividade dos graficos

--- src/Filtro.py
+++ src/Filtro.py
@@ -1,60 +1,9 @@
-from persistencia.conexao import Conexao
 from BuscaBD import BuscaBD
 
 
 class Filtro:
     def __init__(self):
-        self._con = Conexao.Conexao()
         self._busca = BuscaBD()
-
-    def buscar_paciente_sexo(self):
-        conect = self._con.conectar()
-        cursor = conect.cursor(buffered=True)
-        query_select_paciente = "SELECT  p.paciente_sexo, ex.ex_evolucao_caso FROM  paciente as p, exame as ex  where ex. ex_fk_paciente_id=p.paciente_id;"
-
-        cursor.execute(query_select_paciente)
-        result = cursor.fetchall()
-        return result
-
-    def buscar_endereco(self, id_endereco=0, id_paciente=0):
-        conect = self._con.conectar()
-
-        cursor = conect.cursor(buffered=True)
-
-        query_select_endereco = '''SELECT * FROM endereco; '''
-        query_select_by_id = "SELECT * FROM endereco WHERE id= %s;"
-
-        if(id_endereco != 0):
-            val = (id,)
-            cursor.execute(query_select_by_id, val)
-            result = cursor.fetchone()
-
-            return result
-        else:
-            cursor.execute(query_select_endereco)
-            result = cursor.fecthall()
-
-            return result
-
-    def buscar_teste(self, id=0):
-        conect = self._con.conectar()
-        cursor = conect.cursor(buffered=True)
-        query_select_teste = '''SELECT * FROM teste; '''
-        query_select_by_id = "SELECT * FROM endereco WHERE id= %s;"
-
-        if(id != 0):
-            val = (id,)
-            cursor.execute(query_select_by_id, val)
-
-            result = cursor.fecthone()
-
-            return result
-        else:
-            cursor.excute(query_select_teste)
-
-            result = cursor.fetchone()
-
-            return result
 
     def buscar_obtos_sintomas(self):
         conect = self._con.conectar()
@@ -196,13 +145,12 @@ class Filtro:
         total = {}
         infectados = 0
         dados = self._busca.buscar_infectados()
-        # print(dados)
+
         for x in range(len(dados)):
-            # print(dados[0])
+
             if(dados[x][0] == "Positivo"):
-                #print("Valor dos dados", dados)
+
                 infectados = infectados + 1
-               # print(infectados)
 
         total = {'infectados': infectados}
         return total
